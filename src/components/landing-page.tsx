eaa7b0ce8ce488df96a078da45de23d2e4f62207
Aditya Addepalli <adi.addepalli@gmail.com>
2024-11-14 01:36:27 -0500

    feat: adding initial video

--- src/components/landing-page.tsx
+++ src/components/landing-page.tsx
@@ -8,6 +8,21 @@ export default function LandingPage() {
   return (
     <main>
       <Hero />
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          style={{ width: "100%", maxWidth: "300px" }}
+        >
+          <source
+            src="https://utfs.io/f/70ihSaB10mQqmcGew0U8pTF3YZA9qLv1VEfknlRDgzosWB8H"
+            type="video/mp4"
+          />
+          Your browser does not support the video tag.
+        </video>
+      </div>
 
       <HowItWorks />
       <WhoItsFor />
